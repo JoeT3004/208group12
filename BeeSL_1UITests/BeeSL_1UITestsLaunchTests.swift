f0e7f89fe529a21f6e9f8e48786dae09f0e3ac7b
JoeT3004 <joeytime3004@gmail.com>
2024-03-08 11:11:21 +0000

    Initial Commit

---
+++ BeeSL_1UITests/BeeSL_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BeeSL_1UITestsLaunchTests.swift
+//  BeeSL_1UITests
+//
+//  Created by Taylor, Joe [sgjtay21] on 29/02/2024.
+//
+
+import XCTest
+
+final class BeeSL_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
