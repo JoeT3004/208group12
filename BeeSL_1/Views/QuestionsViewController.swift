36fc9d684b47c7594da24e1cb14bf8b37e226d9a
JoeT3004 <joeytime3004@gmail.com>
2024-03-12 15:09:53 +0000

    middle of quiz implementation saving incase something goes wrong.

--- BeeSL_1/Views/QuestionsViewController.swift
+++ BeeSL_1/Views/QuestionsViewController.swift
@@ -1,19 +1,22 @@
 //
-//  ViewController.swift
+//  QuestionsViewController.swift
 //  BeeSL_1
 //
-//  Created by Taylor, Joe [sgjtay21] on 29/02/2024.
+//  Created by Taylor, Joe [sgjtay21] on 12/03/2024.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+
+
+class QuestionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
-}
 
+}
