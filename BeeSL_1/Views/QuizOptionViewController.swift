b623110ce4c8b3211c086f9ccadf392c4f89e068
JoeT3004 <joeytime3004@gmail.com>
2024-03-14 19:18:17 +0000

    Bones of both question types completed!

--- BeeSL_1/Views/QuizOptionViewController.swift
+++ BeeSL_1/Views/QuizOptionViewController.swift
@@ -13,7 +13,9 @@ class QuizOptionViewController: UIViewController {
     
     @IBOutlet weak var tableViewCreatedQuizzes: UITableView!
     
-    let ourQuizzes = ["quiz 1","quiz 2","quiz 3", "quiz 4"]
+    let BSLtoEnglish = ["quiz 1","quiz 2","quiz 3", "quiz 4"]
+    
+    let englishToBSL = ["Quiz a", "Quiz b", "Quiz z"]
     let selfMadeQuizzes = ["Created quiz 1","Created quiz 2","Created quiz 3"]
     
     override func viewDidLoad() {
@@ -28,8 +30,6 @@ class QuizOptionViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-    
-
     /*
     // MARK: - Navigation
 
@@ -46,30 +46,45 @@ class QuizOptionViewController: UIViewController {
 
 extension QuizOptionViewController: UITableViewDelegate, UITableViewDataSource{
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if tableView.tag == 0
         {
-            return ourQuizzes.count
+            return section == 0 ? BSLtoEnglish.count : englishToBSL.count
         }
         else{
             return selfMadeQuizzes.count
         }
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if tableView == tableViewQuizzes {
+            return section == 0 ? "BSL -> English" : "English -> BSL"
+        }
+        return nil
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if tableView.tag == 0
-        {
-            let cell = tableViewQuizzes.dequeueReusableCell(withIdentifier: QuizTableViewCell.identifier, for: indexPath) as! QuizTableViewCell
-            cell.configure(with: ourQuizzes[indexPath.row])
+        if tableView.tag == 0 {
+            
+            let identifier = QuizTableViewCell.identifier
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? QuizTableViewCell else {
+                fatalError("Unable to dequeue a QuizTableViewCell")
+            }
+            let quizName = indexPath.section == 0 ? BSLtoEnglish[indexPath.row] : englishToBSL[indexPath.row]
+            cell.configure(with: quizName)
             cell.delegate = self
             return cell
-        }
-        else{
+        } else {
+            
             let cell = tableViewCreatedQuizzes.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
             cell.textLabel?.text = selfMadeQuizzes[indexPath.row]
             return cell
-            
         }
+        
     }
     
 }
@@ -88,6 +103,13 @@ extension QuizOptionViewController: QuizTableViewDelegate {
             vc.modalPresentationStyle = .fullScreen
             present(vc, animated: true)
             //some functionality here to say: if quiz 1 was picked when reaching the new view controller then quiz one will play. this will be done in the new view controller.
+        }
+        else if title == "Quiz a" {
+            print("\(title)")
+            let vc = storyboard?.instantiateViewController(withIdentifier: "game2") as! Game2ViewController
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true)
+            
         }
 
     }
