fd72c05b1208c621acc2fb0ea062f7b26a39d71d
JoeT3004 <joeytime3004@gmail.com>
2024-03-18 18:43:45 +0000

    made it easier to access quizzes, as both quiz option quesiton data is now in QuizOptionViewController not the game view controllers

---
+++ BeeSL_1/Components/ModelFiles.swift
@@ -0,0 +1,44 @@
+//
+//  ModelFiles.swift
+//  BeeSL_1
+//
+//  Created by Taylor, Joe [sgjtay21] on 18/03/2024.
+//
+
+import Foundation
+
+struct QuestionType1: QuestionTypes {
+    var text: String
+    var answers: [Answer]
+}
+
+struct QuestionType2: QuestionTypes {
+    var text: String
+    var answers: [Answer]
+}
+
+struct Quiz {
+    let title: String
+    let type: QuizType
+    let questions: [QuestionTypes]
+}
+
+
+struct Answer {
+    let text: String
+    let correct: Bool
+}
+
+enum QuizType {
+    case BSLtoEnglish
+    case EnglishtoBSL
+}
+
+
+protocol QuestionTypes{
+    var text: String { get }
+    var answers: [Answer] { get }
+    //hand gesture variable
+}
+
+
