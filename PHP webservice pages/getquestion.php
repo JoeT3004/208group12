17209d1eff3a8cf03c33ac08b3f3c6f4329ef46d
svtstickypiston <102304646+svtstickypiston@users.noreply.github.com>
2024-04-03 22:09:39 +0100

    Create getquestion.php

---
+++ PHP webservice pages/getquestion.php
@@ -0,0 +1,57 @@
+<?php
+
+$DATABASE_HOST = 'studdb.csc.liv.ac.uk';
+$DATABASE_USER = 'sgtbrett';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'sgtbrett';
+
+$id = $_GET['id'];
+
+// Create connection
+$con=mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+if ( !isset($id) ) {
+        // Couldn't access the id
+        exit('No question inputted...');
+}
+
+
+
+// searches the users table for a user with a matching username
+$sql = "SELECT Prompt, CorrectAnswer, WrongAnswer1, WrongAnswer2, WrongAnswer3 FROM Questions WHERE QuestionID=?";
+
+// Check if there are results
+if ($stmt = $con->prepare($sql))
+{
+        // If so, then create a results array and a temporary one
+        // to hold the data
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+                $stmt->bind_result($prompt, $correct, $wrong1, $wrong2, $wrong3);
+                $stmt->fetch();
+
+                $row = array($prompt, $correct, $wrong1, $wrong2, $wrong3);
+                echo json_encode($row);
+
+        } else {
+                // Incorrect id
+                echo 'Question ID does not exist';
+        }
+}
+else {
+        echo "SQL Statement Failed";
+}
+
+// Close connections
+mysqli_close($con);
+?>
