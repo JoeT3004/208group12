7ceb100cd2c975ff5695d4c696d451c03815a38c
svtstickypiston <102304646+svtstickypiston@users.noreply.github.com>
2024-04-18 12:37:59 +0100

    Create getbsltoeng.php

---
+++ PHP webservice pages/getbsltoeng.php
@@ -0,0 +1,61 @@
+<?php
+
+$DATABASE_HOST = 'studdb.csc.liv.ac.uk';
+$DATABASE_USER = 'sgtbrett';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'sgtbrett';
+
+$id = $_GET['id'];
+
+// Create connection
+$con=mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+if ( !isset($id) ) {
+        // Couldn't access the id
+        exit('No question inputted...');
+}
+
+// searches the questions table for a question with a matching id
+$sql = "SELECT QuestionsBSLtoENG.Prompt, QuestionsBSLtoENG.CorrectAnswer, QuestionsBSLtoENG.WrongAnswer1, QuestionsBSLtoENG.WrongAnswer2, QuestionsBSLtoENG.WrongAnswer3 FROM QuestionsBSLtoENG, BSLtoENGonQuiz, Quizzes WHERE BSLtoENGonQuiz.QuestionID=QuestionsBSLtoENG.QuestionID AND Quizzes.QuizID=? AND BSLtoENGonQuiz.QuizID=?";
+$resultArray = array();
+
+// Check if there are results
+if ($stmt = $con->prepare($sql))
+{
+        // If so, then create a results array and a temporary one
+        // to hold the data
+        $stmt->bind_param('ii', $id, $id);
+        $stmt->execute();
+
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+                $stmt->bind_result($prompt, $correct, $wrong1, $wrong2, $wrong3);
+
+                while($stmt->fetch())
+                {
+                        $tempArray = array($prompt, $correct, $wrong1, $wrong2, $wrong3);
+                        array_push($resultArray, $tempArray);
+
+                        $stmt->bind_result($prompt, $correct, $wrong1, $wrong2, $wrong3);
+                }
+                // echo json_encode($row);
+
+        }
+
+}
+else {
+        echo "SQL Statement Failed";
+}
+
+echo json_encode($resultArray);
+
+// Close connections
+mysqli_close($con);
+?>
